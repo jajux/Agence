78e5df0eac148052a89b0ceb5f970b8af7d2511b
Jawad EL HATTAB <jawad.elhattab@hotmail.fr>
2021-11-30 09:08:33 +0100

    setup responsive home section & animation background home

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
+// hamburgerMenu activation
 const hamburgerMenu = document.querySelector('.btn-burger');
 const nav = document.querySelector('.nav-left');
-const navLinks = document.querySelector('.nav-menu-item');
+const navLinks = document.querySelectorAll('.nav-menu-item');
 const line = document.querySelector('.cont-burger');
 
-hamburgerMenu.addEventListener('click', ()=> {
-    
-    line.classList.toggle('line-active');
-    nav.classList.toggle('menu-visible');
+hamburgerMenu.addEventListener('click', () => {
+
+    line.classList.toggle('active')
+    nav.classList.toggle('menu-visible')
+
 })
 
-if (window.matchMedia('(max-width:1300px)')) {
+// Toggle menu media under 1300px
+if (window.matchMedia('(max-width: 1300px)')) {
 
     navLinks.forEach(item => {
-
-        item.addEventListener('click', ()=> {
-            
-            nav.classList.toggle('menu-visible');
-            line.classList.toggle('line-active');
+        item.addEventListener('click', () => {
+            nav.classList.toggle('menu-visible')
+            line.classList.toggle('active');
         })
     })
+
 }
-    
+
+
